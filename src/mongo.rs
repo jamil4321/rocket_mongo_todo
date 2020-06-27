956d4663e66c6add5fb8dcca22a4eee1164b737d
Jamil Ahmed <jamil.ahmed.ja28@gmail.com>
2020-06-28 02:58:14 +0500

    Update mongo.rs

--- src/mongo.rs
+++ src/mongo.rs
@@ -42,8 +42,9 @@ impl InsertableTodo{
 const TODO:&str = "todo_item";
 
 fn mongo_connection(coll:&str)->Result<Collection, Error>{
-    let client = Client::with_uri_str("mongodb://localhost:27017")?;
+    //let client = Client::with_uri_str("mongodb://localhost:27017")?;
     //let client = Client::with_uri_str("mongodb+srv://monodbUser:abc@myfisrtlerningapp-zbcua.mongodb.net/<dbname>?retryWrites=true&w=majority")?;
+    let client = Client::with_uri_str("mongodb://ubysak1nxbrx6pllwqze:ZFLMw1swQ4KfqBG3OqF4@bnrn7mx78ld977k-mongodb.services.clever-cloud.com:27017/bnrn7mx78ld977k");
     let db = client.database("todo");
     let collection = db.collection(coll);
     Ok(collection)
